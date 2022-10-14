0c8aa58327e0286c261360392336bee90268be1e
Aimurok123456 <115090521+Aimurok123456@users.noreply.github.com>
2022-10-15 03:40:59 +0600

    Add files via upload

--- add.js
+++ add.js
@@ -20,3 +20,17 @@ let months = [
 let date = new Date();
 let month = date.getMonth();
 alert(months [month]);
+
+// Задача №3
+// Повторите поведение кнопки по нажатию на нее
+// (она осуществляет обмен содержимым между двумя инпутами,
+// можете понажимать на нее несколько раз
+// или вручную сменить содержимое инпутов)
+function buttonClick() {
+    let input1 = document.getElementById('input1');
+    let input2 = document.getElementById('input2');
+    let input1Value = input1.value;
+    let input2Value = input2.value;
+    input1.value = input2Value;
+    input2.value = input1Value;
+}
